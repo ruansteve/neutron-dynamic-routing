f4bf1aeca8006fe37538292611c0737ede0c8eac
JunoZhu <nazhu@cn.ibm.com>
2016-04-25 02:44:40 -0400

    Move cli out of neutron

---
+++ neutron_dynamic_routing/neutronclient/neutron/v2_0/bgp/dragentscheduler.py
@@ -0,0 +1,152 @@
+# Copyright 2016 Huawei Technologies India Pvt. Ltd.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+from __future__ import print_function
+
+from neutronclient._i18n import _
+from neutronclient.common import extension
+from neutronclient.neutron import v2_0 as neutronV20
+from neutron_dynamic_routing.neutronclient.neutron.v2_0.bgp import speaker
+
+
+BGP_DRINSTANCES = "/bgp-drinstances"
+BGP_DRINSTANCE = "/bgp-drinstance/%s"
+BGP_DRAGENTS = "/bgp-dragents"
+BGP_DRAGENT = "/bgp-dragents/%s"
+
+def add_bgp_speaker_to_dragent(client, bgp_dragent, body):
+    """Adds a BGP speaker to Dynamic Routing agent."""
+    return client.post((client.agent_path + BGP_DRINSTANCES)
+                       % bgp_dragent, body=body)
+
+def remove_bgp_speaker_from_dragent(client, bgp_dragent, bgpspeaker_id):
+    """Removes a BGP speaker from Dynamic Routing agent."""
+    return client.delete((client.agent_path + BGP_DRINSTANCES + "/%s")
+                         % (bgp_dragent, bgpspeaker_id))
+
+def list_bgp_speaker_on_dragent(client, bgp_dragent, **_params):
+    """Fetches a list of BGP speakers hosted by Dynamic Routing agent."""
+    return client.get((client.agent_path + BGP_DRINSTANCES)
+                      % bgp_dragent, params=_params)
+
+def list_dragents_hosting_bgp_speaker(client, bgp_speaker, **_params):
+    """Fetches a list of Dynamic Routing agents hosting a BGP speaker."""
+    return client.get((client.bgp_speaker_path + BGP_DRAGENTS)
+                      % bgp_speaker, params=_params)
+
+
+def add_common_args(parser):
+    parser.add_argument('dragent_id',
+                        metavar='BGP_DRAGENT_ID',
+                        help=_('ID of the Dynamic Routing agent.'))
+    parser.add_argument('bgp_speaker',
+                        metavar='BGP_SPEAKER',
+                        help=_('ID or name of the BGP speaker.'))
+
+
+class AddBGPSpeakerToDRAgent(extension.NeutronClientExtension):
+    """Add a BGP speaker to a Dynamic Routing agent."""
+    
+    shell_command = 'bgp-dragent-speaker-add'
+
+    def get_parser(self, prog_name):
+        parser = super(AddBGPSpeakerToDRAgent, self).get_parser(prog_name)
+        add_common_args(parser)
+        return parser
+
+    def take_action(self, parsed_args):
+        neutron_client = self.get_client()
+        _speaker_id = speaker.get_bgp_speaker_id(neutron_client,
+                                                 parsed_args.bgp_speaker)
+        add_bgp_speaker_to_dragent(neutron_client,
+            parsed_args.dragent_id, {'bgp_speaker_id': _speaker_id})
+        print(_('Associated BGP speaker %s to the Dynamic Routing agent.')
+              % parsed_args.bgp_speaker, file=self.app.stdout)
+
+
+class RemoveBGPSpeakerFromDRAgent(extension.NeutronClientExtension):
+    """Removes a BGP speaker from a Dynamic Routing agent."""
+
+    shell_command = 'bgp-dragent-speaker-remove'
+
+    def get_parser(self, prog_name):
+        parser = super(RemoveBGPSpeakerFromDRAgent, self).get_parser(
+            prog_name)
+        add_common_args(parser)
+        return parser
+
+    def take_action(self, parsed_args):
+        neutron_client = self.get_client()
+        _speaker_id = speaker.get_bgp_speaker_id(neutron_client,
+                                                 parsed_args.bgp_speaker)
+        remove_bgp_speaker_from_dragent(neutron_client, parsed_args.dragent_id,
+                                                       _speaker_id)
+        print(_('Disassociated BGP speaker %s from the '
+                'Dynamic Routing agent.')
+              % parsed_args.bgp_speaker, file=self.app.stdout)
+
+
+class ListBGPSpeakersOnDRAgent(extension.ClientExtensionList):
+    """List BGP speakers hosted by a Dynamic Routing agent."""
+    
+    shell_command = 'bgp-speaker-list-on-dragent'
+
+    list_columns = ['id', 'name', 'local_as', 'ip_version']
+    resource = speaker.BGP_SPEAKER_RESOURCE
+
+    def get_parser(self, prog_name):
+        parser = super(ListBGPSpeakersOnDRAgent,
+                       self).get_parser(prog_name)
+        parser.add_argument(
+            'dragent_id',
+            metavar='BGP_DRAGENT_ID',
+            help=_('ID of the Dynamic Routing agent.'))
+        return parser
+
+    def call_server(self, neutron_client, search_opts, parsed_args):
+        data = list_bgp_speaker_on_dragent(neutron_client,
+            parsed_args.dragent_id, **search_opts)
+        return data
+
+
+class ListDRAgentsHostingBGPSpeaker(extension.ClientExtensionList):
+    """List Dynamic Routing agents hosting a BGP speaker."""
+
+    shell_command = 'bgp-dragent-list-hosting-speaker'
+
+    resource = 'agent'
+    _formatters = {}
+    list_columns = ['id', 'host', 'admin_state_up', 'alive']
+    unknown_parts_flag = False
+
+    def get_parser(self, prog_name):
+        parser = super(ListDRAgentsHostingBGPSpeaker,
+                       self).get_parser(prog_name)
+        parser.add_argument('bgp_speaker',
+                            metavar='BGP_SPEAKER',
+                            help=_('ID or name of the BGP speaker.'))
+        return parser
+
+    def extend_list(self, data, parsed_args):
+        for agent in data:
+            agent['alive'] = ":-)" if agent['alive'] else 'xxx'
+
+    def call_server(self, neutron_client, search_opts, parsed_args):
+        _speaker_id = speaker.get_bgp_speaker_id(neutron_client,
+                                                     parsed_args.bgp_speaker)
+        search_opts['bgp_speaker'] = _speaker_id
+        data = list_dragents_hosting_bgp_speaker(neutron_client, **search_opts)
+        return data
---
+++ neutron_dynamic_routing/neutronclient/neutron/v2_0/bgp/peer.py
@@ -0,0 +1,142 @@
+# Copyright 2016 Huawei Technologies India Pvt. Ltd.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+from neutronclient._i18n import _
+from neutronclient.common import exceptions
+from neutronclient.common import extension
+from neutronclient.common import utils
+from neutronclient.common import validators
+from neutronclient.neutron import v2_0 as neutronv20
+#from neutron_dynamic_routing.neutronclient.neutron.v2_0.bgp import speaker
+
+BGP_PEER_RESOURCE = 'bgp_peer'
+BGP_PEERS_PATH = '/bgp-peers'
+BGP_PEER_PATH = '/bgp-peers/%s'
+
+def get_bgp_peer_id(client, id_or_name):
+    return neutronv20.find_resourceid_by_name_or_id(client,
+                                                    'bgp_peer',
+                                                    id_or_name)
+
+
+def validate_peer_attributes(parsed_args):
+    # Validate AS number
+    validators.validate_int_range(parsed_args, 'remote_as',
+                                  speaker.MIN_AS_NUM,
+                                  speaker.MAX_AS_NUM)
+    # Validate password
+    if parsed_args.auth_type != 'none' and parsed_args.password is None:
+        raise exceptions.CommandError(_('Must provide password if auth-type '
+                                        'is specified.'))
+    if parsed_args.auth_type == 'none' and parsed_args.password:
+        raise exceptions.CommandError(_('Must provide auth-type if password '
+                                        'is specified.'))
+
+class BGPPeer(extension.NeutronClientExtension):
+    resource = BGP_PEER_RESOURCE
+    resource_plural = '%ss' % resource
+
+    #object_path = '/%s' % resource_plural
+    #resource_path = '/%s/%%s' % resource_plural
+    object_path = BGP_PEERS_PATH
+    resource_path = BGP_PEER_PATH
+
+    versions = ['2.0']
+
+class ListPeers(BGPPeer, extension.ClientExtensionList):
+    """List BGP peers."""
+
+    shell_command = 'bgp-peer-list'
+    list_columns = ['id', 'name', 'peer_ip', 'remote_as']
+    pagination_support = True
+    sorting_support = True
+
+
+class ShowPeer(BGPPeer, extension.ClientExtensionShow):
+    """Show information of a given BGP peer."""
+
+    shell_command = 'bgp-peer-show'
+
+
+class CreatePeer(BGPPeer, extension.ClientExtensionCreate):
+    """Create a BGP Peer."""
+
+    shell_command = 'bgp-peer-create'
+
+    def add_known_arguments(self, parser):
+        parser.add_argument(
+            'name',
+            metavar='NAME',
+            help=_('Name of the BGP peer to create.'))
+        parser.add_argument(
+            '--peer-ip',
+            metavar='PEER_IP_ADDRESS',
+            required=True,
+            help=_('Peer IP address.'))
+        parser.add_argument(
+            '--remote-as',
+            required=True,
+            metavar='PEER_REMOTE_AS',
+            help=_('Peer AS number. (Integer in [%(min_val)s, %(max_val)s] '
+                   'is allowed.)') %
+            {'min_val': speaker.MIN_AS_NUM,
+             'max_val': speaker.MAX_AS_NUM})
+        parser.add_argument(
+            '--auth-type',
+            metavar='PEER_AUTH_TYPE',
+            choices=['none', 'md5'],
+            default='none',
+            type=utils.convert_to_lowercase,
+            help=_('Authentication algorithm. Supported algorithms: '
+                   'none(default), md5'))
+        parser.add_argument(
+            '--password',
+            metavar='AUTH_PASSWORD',
+            help=_('Authentication password.'))
+
+    def args2body(self, parsed_args):
+        body = {}
+        validate_peer_attributes(parsed_args)
+        neutronv20.update_dict(parsed_args, body,
+                               ['name', 'peer_ip',
+                                'remote_as', 'auth_type', 'password'])
+        return {self.resource: body}
+
+
+class UpdatePeer(BGPPeer, extension.ClientExtensionUpdate):
+    """Update BGP Peer's information."""
+
+    shell_command = 'bgp-peer-update'
+
+    def add_known_arguments(self, parser):
+        parser.add_argument(
+            '--name',
+            help=_('Updated name of the BGP peer.'))
+        parser.add_argument(
+            '--password',
+            metavar='AUTH_PASSWORD',
+            help=_('Updated authentication password.'))
+
+    def args2body(self, parsed_args):
+        body = {}
+        neutronv20.update_dict(parsed_args, body, ['name', 'password'])
+        return {self.resource: body}
+
+
+class DeletePeer(BGPPeer, extension.ClientExtensionDelete):
+    """Delete a BGP peer."""
+
+    shell_command = 'bgp-peer-delete'
